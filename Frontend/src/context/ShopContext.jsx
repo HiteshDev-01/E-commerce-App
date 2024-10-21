259280f640ce17582a9685b63e6340bc65b778ca
HiteshDev-01 <hiteshpalival01@gmail.com>
2024-10-21 22:37:10 +0530

    Remove backend import statment from the frontend.

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { User } from "../../../Backend/models/user.model";
 
 export const shopContext = createContext();
 
