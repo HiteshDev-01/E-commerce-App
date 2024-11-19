df20e41f6025abbfd9b0f416cdd870756e1e9a9c
HiteshDev-01 <hiteshpalival01@gmail.com>
2024-11-19 18:54:31 +0530

    Changes in cors origin

--- Backend/server.js
+++ Backend/server.js
@@ -11,21 +11,27 @@ connectDB();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin:"https://clothesmaniaadmin.vercel.app", credentials:true }));
+app.use(
+  cors({
+    origin: "https://clothesmaniaadmin.vercel.app", // Allow requests only from this origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific methods
+    credentials: true, // Allow cookies and credentials if needed
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/",(req,res) => {
-  res.send("API IS WORKING")
-})
+app.use("/", (req, res) => {
+  res.send("API IS WORKING");
+});
 
 import userRouter from "./routes/user.routes.js";
 import productRouter from "./routes/product.routes.js";
 import cartRouter from "./routes/cart.routes.js";
-import orderRouter from './routes/order.routes.js';
+import orderRouter from "./routes/order.routes.js";
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
-app.use("/api/cart",cartRouter);
+app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 // Connect to DB and start the server
